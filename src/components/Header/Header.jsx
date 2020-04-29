72fa93c3d682a020553fd7f8a058a97638737774
mustran <musli.ademi@inplayer.com>
2020-04-29 22:40:58 +0200

    Add header component as test

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <ul>
+            <li>AL</li>
+            <li>EN</li>
+            <li>MK</li>
+            <li>MENU</li>
+            <li>CONTACT</li>
+            <li>LOG IN</li>
+        </ul>
+    )
+}
+
+export default Header;
