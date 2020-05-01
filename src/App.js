853d41e5e7f234b9666ab5fa1e073e090ca396ad
mustran <musli.ademi@inplayer.com>
2020-05-01 23:11:13 +0200

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 
-const App = () => <h1>Welcome</h1>;
+const HomePage = () => <div style={{ color: 'white' }}>Homepage</div>;
+
+const PageNotFound = () => <div>Page not found</div>;
+const App = () => {
+    return (
+        <>
+            <Header />;
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Route>
+                    <PageNotFound />
+                </Route>
+            </Switch>
+        </>
+    );
+};
 
 export default App;
