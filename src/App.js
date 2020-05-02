1cf27d4a73386e3d55b54a54837a49abea554881
mustran <musli.ademi@inplayer.com>
2020-05-02 18:18:09 +0200

    Implement routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import HomePage from './components/pages/Home/HomePage';
+import Menu from './components/pages/Menu/Menu';
+import { LineBreak, Copyright } from './components/Footer/Footer';
 
-const HomePage = () => <div style={{ color: 'white' }}>Homepage</div>;
+const AppWrapper = styled.div`
+    width: 80%;
+    margin: auto;
+    text-align: center;
+`;
 
 const PageNotFound = () => <div>Page not found</div>;
 const App = () => {
     return (
-        <>
-            <Header />;
+        <AppWrapper>
+            <Header />
             <Switch>
                 <Route exact path="/">
                     <HomePage />
+                </Route>
+                <Route exact path="/menu">
+                    <Menu />
                 </Route>
                 <Route>
                     <PageNotFound />
                 </Route>
             </Switch>
-        </>
+            <LineBreak top={10} bottom={20} />
+            <Copyright />
+        </AppWrapper>
     );
 };
 
