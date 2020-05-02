c825c115465829b6006c3961c7a214db67c56e39
mustran <musli.ademi@inplayer.com>
2020-05-02 18:15:10 +0200

    Add navigation items

---
+++ src/components/Header/NavItems/NavItems.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import uuid from 'uuid/dist/v4';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import colors from '../../../theme/colors';
+
+const active = 'active';
+
+const StyledNavItem = styled(NavLink).attrs({ active })`
+    color: ${colors.gray};
+    font-size: ${(props) => props.theme.sizes.large};
+    text-decoration: none;
+    text-transform: uppercase;
+    margin-left: 20px;
+    font-weight: ${(props) => props.theme.weights.bold};
+    &.${active} {
+        color: ${colors.garden};
+    }
+`;
+
+const NavItems = ({ items }) =>
+    items.map((item) => (
+        <StyledNavItem to={item.to} key={uuid()}>
+            {item.navLink}
+        </StyledNavItem>
+    ));
+
+export default NavItems;
