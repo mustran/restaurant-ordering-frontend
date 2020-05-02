bbed8910b44df2e4708e6bde173ddd530298bd1f
mustran <musli.ademi@inplayer.com>
2020-05-02 19:22:11 +0200

    Add order section styles

--- src/components/pages/Menu/Order/OrderSection.jsx
+++ src/components/pages/Menu/Order/OrderSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import colors from '../../../../theme/colors';
+import { FaShoppingBag } from 'react-icons/fa';
 
 const StyledOrder = styled.div`
     background-color: ${colors.garden};
@@ -9,15 +10,28 @@ const StyledOrder = styled.div`
     border-radius: 10px;
 `;
 
+const YourOrder = styled.h2`
+    color: white;
+`;
+
 const OrderButton = styled.button`
     color: ${colors.white};
     background-color: ${colors.backgroundColor};
+    font-weight: ${(props) => props.theme.weights.bold};
+    width: 95%;
+    height: 35px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
     text-transform: uppercase;
 `;
 
 const OrderSection = () => {
     return (
         <StyledOrder>
+            <YourOrder>
+                <FaShoppingBag /> Your order
+            </YourOrder>
             <OrderButton>Log in</OrderButton>
         </StyledOrder>
     );
