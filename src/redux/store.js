2737e691e05f503c698f025c1a451f9301fde956
mustran <musli.ademi@inplayer.com>
2020-05-03 23:04:03 +0200

    Add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+// root reducer
+import rootReducer from './rootReducer';
+
+const middlewares = [thunk];
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(...middlewares),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+export default store;
