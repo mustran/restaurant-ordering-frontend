bec49c424ce1d27da65867800f9fac99b251651d
mustran <musli.ademi@inplayer.com>
2020-05-03 23:03:11 +0200

    Wrap application with redux store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ThemeWrapper from './theme/ThemeWrapper';
 import { theme } from './theme/theme';
+import store from './redux/store';
 
 ReactDOM.render(
-    <ThemeWrapper theme={theme}>
-        <React.StrictMode>
-            <Router>
-                <App />
-            </Router>
-        </React.StrictMode>
-    </ThemeWrapper>,
+    <Provider store={store}>
+        <ThemeWrapper theme={theme}>
+            <React.StrictMode>
+                <Router>
+                    <App />
+                </Router>
+            </React.StrictMode>
+        </ThemeWrapper>
+    </Provider>,
     document.getElementById('root')
 );
 
