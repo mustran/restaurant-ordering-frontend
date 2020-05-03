c2435d4cb6c4cd20764430bed5b5fea51bd6a4e6
mustran <musli.ademi@inplayer.com>
2020-05-03 23:06:51 +0200

    Add login route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Menu from './components/pages/Menu/Menu';
 import Footer from './components/Footer/Footer';
 import Contact from './components/pages/Contact/Contact';
 import ProtectedRoute from './routes/ProtectedRoute';
+import { getIsLoggedIn } from './redux/auth/reducer';
+import { connect } from 'react-redux';
+import LogIn from './components/pages/LogIn/LogIn';
 
 const AppWrapper = styled.div`
     width: 80%;
@@ -22,7 +25,8 @@ const ContentWrapper = styled.div`
 `;
 
 const PageNotFound = () => <div>Page not found</div>;
-const App = () => {
+
+const App = ({ isLoggedIn }) => {
     return (
         <AppWrapper>
             <ContentWrapper>
@@ -35,12 +39,19 @@ const App = () => {
                         exact
                         path="/menu"
                         // get user from redux
-                        user={false}
+                        authenticated={isLoggedIn}
                         component={Menu}
                     />
                     <Route exact path="/contact">
                         <Contact />
                     </Route>
+                    <Route
+                        exact
+                        path="/login"
+                        render={(props) => (
+                            <LogIn authenticated={isLoggedIn} {...props} />
+                        )}
+                    />
                     <Route>
                         <PageNotFound />
                     </Route>
@@ -51,4 +62,8 @@ const App = () => {
     );
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+    isLoggedIn: getIsLoggedIn(state),
+});
+
+export default connect(mapStateToProps, null)(App);
