91cef72aae47f35a7c64ce5c96069903eef93a39
mustran <musli.ademi@inplayer.com>
2020-05-11 22:42:32 +0200

    Add fetch products async function to get products from database

--- src/redux/products/fetchProducts.js
+++ src/redux/products/fetchProducts.js
@@ -5,4 +5,21 @@ import {
     fetchProductsError,
 } from './actions';
 
-const url = 'http://localhost:8080/api/menu/categories/1/products';
+export const fetchProductsByCategory = (categoryId) => {
+    console.log('CATEGORY ID');
+    console.log(categoryId);
+    const url = `http://localhost:8080/api/menu/categories/${categoryId}/products`;
+
+    return async (dispatch) => {
+        dispatch(fetchProductsPreload());
+        try {
+            let response = await axios(url);
+            const data = response.data;
+            console.log('FETCH CATEGORY');
+            console.log(data);
+            dispatch(fetchProductsSuccess(data));
+        } catch (e) {
+            dispatch(fetchProductsError());
+        }
+    };
+};
