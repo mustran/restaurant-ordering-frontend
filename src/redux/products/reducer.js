ee730882b08a279c06e6361573602a6300b8813e
mustran <musli.ademi@inplayer.com>
2020-05-11 22:45:40 +0200

    Remove mock products data

--- src/redux/products/reducer.js
+++ src/redux/products/reducer.js
@@ -2,84 +2,13 @@ import {
     FETCH_PRODUCTS_ERROR,
     FETCH_PRODUCTS_PRELOAD,
     FETCH_PRODUCTS_SUCCESS,
-    FETCH_DYNAMIC_PRODUCTS,
     ACTIVE_CATEGORY,
 } from './actions';
 
-const mockProductsData = {
-    Breakfast: [
-        {
-            productName: 'Breakfast1',
-            description: '2 fried eggs, 2 chicken sausages',
-            price: 140,
-        },
-        {
-            productName: 'Breakfast2',
-            description:
-                '5 fried eggs, 5 chicken sausages, white cheese, 2 cucumber, tomatoes, olives, honey',
-            price: 440,
-        },
-        {
-            productName: 'Breakfast3',
-            description:
-                '2 fried eggs, 2 chicken sausages, white cheese, 2 cucumber,',
-            price: 340,
-        },
-        {
-            productName: 'Breakfast4',
-            description:
-                '5 fried eggs, 5 chicken sausages, white cheese, 2 cucumber, tomatoes, olives, honey',
-            price: 540,
-        },
-        {
-            productName: 'Breakfast3',
-            description:
-                '2 fried eggs, 2 chicken sausages, white cheese, 2 cucumber,',
-            price: 340,
-        },
-        {
-            productName: 'Breakfast4',
-            description:
-                '5 fried eggs, 5 chicken sausages, white cheese, 2 cucumber, tomatoes, olives, honey',
-            price: 540,
-        },
-        {
-            productName: 'Breakfast3',
-            description:
-                '2 fried eggs, 2 chicken sausages, white cheese, 2 cucumber,',
-            price: 340,
-        },
-        {
-            productName: 'Breakfast4',
-            description:
-                '5 fried eggs, 5 chicken sausages, white cheese, 2 cucumber, tomatoes, olives, honey',
-            price: 540,
-        },
-    ],
-    Salad: [
-        {
-            productName: 'Salad1',
-            description: 'salad description 1',
-            price: 240,
-        },
-        {
-            productName: 'Salad2',
-            description: 'salad description 2',
-            price: 240,
-        },
-        {
-            productName: 'Salad3',
-            description: 'salad description 3',
-            price: 440,
-        },
-    ],
-    Meze: [7, 8, 9],
-};
-
 const initialState = {
     preload: false,
-    products: mockProductsData.Breakfast,
-    active: Object.keys(mockProductsData)[0],
+    products: [],
+    active: '',
 };
 
 const productsReducer = (state = initialState, action) => {
@@ -92,7 +21,7 @@ const productsReducer = (state = initialState, action) => {
         case FETCH_PRODUCTS_SUCCESS: {
             return {
                 ...state,
-                products: action.payload.products,
+                products: action.payload,
             };
         }
         case FETCH_PRODUCTS_ERROR:
@@ -100,12 +29,6 @@ const productsReducer = (state = initialState, action) => {
                 ...state,
                 preload: false,
             };
-        case FETCH_DYNAMIC_PRODUCTS: {
-            return {
-                ...state,
-                products: mockProductsData[action.payload],
-            };
-        }
         case ACTIVE_CATEGORY: {
             return {
                 ...state,
